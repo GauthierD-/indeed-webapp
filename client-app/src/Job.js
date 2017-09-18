d74d69bb384c231be42388dd3234b599dfcecb27
Gauthier Deroo <g.deroo@gmail.com>
2017-09-18 18:36:54 -0400

    feat Update Job Components with isSaved job

--- client-app/src/Job.js
+++ client-app/src/Job.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import './job.css'
 
-function Job ({ infos }) {
-  console.log('infoss=', infos)
+function Job ({ infos, saveJob, deleteJob }) {
+  const style = {
+    backgroundColor: 'aliceblue'
+  }
+
+  if (infos.isSaved) {
+    style.backgroundColor = '#00ffc4'
+  }
+
   return (
-    <div className='job_box'>
-      <h2>{infos.title}</h2>
-      <p>Date: {new Date(infos.date).toUTCString()} - Salary {infos.salary}</p>
+    <div className='job_box' style={style}>
+      <h2>{infos.name}</h2>
+      <p>Date: {new Date(infos.date).toUTCString()} - Salary {infos.salaryInterval} - {infos.salary} </p>
       <p>{infos.description}</p>
+      {
+        infos.isSaved
+          ? <button onClick={() => deleteJob(infos)}>Delete</button>
+          : <button onClick={() => saveJob(infos)}>Save</button>
+      }
     </div>
   )
 }
