820acc93d2363b4b837356387936503c3be8b92f
Gauthier Deroo <g.deroo@gmail.com>
2017-09-18 18:34:57 -0400

    chore update Server config

--- server/config/index.js
+++ server/config/index.js
@@ -8,7 +8,11 @@ nconf
   .file('conf', { file: 'env.json', dir: path.join(__dirname, '../etc/conf'), search: true })
   .defaults({
     'PORT': 8000,
-    'REDIS_URL': 'redis'
+    'REDIS_URL': 'redis',
+    'API_URI': 'https://data.usajobs.gov/api/search',
+    'API_HOST': 'data.usajobs.gov',
+    'API_UA': 'my@email.com',
+    'API_KEY': 'ar///huZ8jYHmO731jJBT9K//92bUUHBn3Q4KVi6VxM='
   })
 
 module.exports = nconf
